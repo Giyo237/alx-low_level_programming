c2515c314e9bfa334b1d38c20faf423bad264fae
kalifa <kalifkalifa@gmail.com>
2023-09-07 10:12:48 -0700

    qoute.c'

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - simple c prog that prints a line of string
+ * Return: 1 if succesful
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+return (1);
+}
