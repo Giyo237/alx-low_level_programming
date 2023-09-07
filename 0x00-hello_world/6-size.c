86b81e10e23ff1c0ab735a1779535a20f2106fa4
kalifa <kalifkalifa@gmail.com>
2023-09-07 08:50:00 -0700

    size of

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - program that prints the computer sizes
+ * Return: 0 if succeful
+ */
+int main(void)
+{
+	char a;
+	int i;
+	long int l;
+	long long int v;
+	float f;
+printf("Size of a char: %u bytes(s)\n", a);
+printf("Size of a int: %u bytes(s)\n", i);
+printf("Size of a long int: %u bytes(s)\n", l);
+printf("Size of a long long int: %u bytes(s)\n", v);
+printf("Size of a float: %u bytes(s)\n", f);
+return (0);
+}
