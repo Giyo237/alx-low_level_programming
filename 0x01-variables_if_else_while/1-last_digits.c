5190ea602814ae3a88c424195a7ff746c880a704
kalifa <kalifkalifa@gmail.com>
2023-09-08 13:37:27 -0700

    corected iso error

--- 0x01-variables_if_else_while/1-last_digits.c
+++ 0x01-variables_if_else_while/1-last_digits.c
@@ -16,12 +16,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	r = n % 10;
-	if (m > 5)
+	if (n > 5)
 		printf("Last digits of %d is %d and is greater than 5\n", n, r);
 	else if (n == 0)
 		printf("Last digit of %d is %d and is 0\n", n, r);
 	else if (n < 6)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, r);
-
-	return (0);
+return (0);
 }
