75b5edd03911372af3b0f53dbdd2301db5c6fa77
kalifa <kalifkalifa@gmail.com>
2023-09-08 16:26:48 -0700

    z to a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet in rev
+ * Return: 0 if it runs succesfully
+ */
+int main(void)
+{
+	char i;
+
+	for (i < 'a'; i <= 'z'; i--)
+		puchar(i);
+	putchar('\n);
+	return (0);
+}
