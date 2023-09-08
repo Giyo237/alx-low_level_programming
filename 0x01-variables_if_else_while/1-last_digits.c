e060398132d3c72b856359f58320391099dc9d0d
kalifa <kalifkalifa@gmail.com>
2023-09-08 13:49:06 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digits.c
+++ 0x01-variables_if_else_while/1-last_digits.c
@@ -20,8 +20,7 @@ int main(void)
 		printf("Last digits of %d is %d and is greater than 5\n", n, r);
 	else if (r == 0)
 		printf("Last digit of %d is %d and is 0\n", n, r);
-	else
-	(r <6 && r!=0)
+	else if (r < 6 && r != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, r);
 	return (0);
 }
