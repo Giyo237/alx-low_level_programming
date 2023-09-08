9783d96f55950b9cd731ef73689c299d43ccc27e
kalifa <kalifkalifa@gmail.com>
2023-09-08 16:46:26 -0700

    hexa

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,10 +7,10 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10 i++)
+	for (i = 0; i < 10; i++)
 		putchar(i + '0');
 	for (i = 10; i < 16; i++)
-		putchar(i + 'a')
+		putchar(i + 'a');
 			putchar('\n');
 	return (0);
 }
