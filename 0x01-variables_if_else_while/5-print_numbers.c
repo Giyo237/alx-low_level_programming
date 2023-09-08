95f6b530126be998cc4722db1faf8160bcc1c77c
kalifa <kalifkalifa@gmail.com>
2023-09-08 15:29:32 -0700

    intergers in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - c program that prints numbers from 0 to 9
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+		putchar(i);
+			putchar('\n');
+			return (0);
+}
