4b81568577d9be1c177bda375e3fd61213467b1c
kalifa <kalifkalifa@gmail.com>
2023-09-08 14:24:47 -0700

    leters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - a program that prints alphabets in lower case
+ * Return: 0 if succeful
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		printf("%c \n", i);
+	return (0);
+}
+
