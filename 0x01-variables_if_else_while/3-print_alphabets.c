12e5de516cc668dacc333264a8c4034bcf71e334
kalifa <kalifkalifa@gmail.com>
2023-09-08 14:44:29 -0700

    ato z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A program that prints from a to z in both upper and lower cases
+ * Return: 0 when succesful
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z' i++)
+		printf("%c", i);
+	printf("\n\n");
+	for (i = 'A'; i <= 'Z' i++)
+		printf("%c", i);
+	printf("\n");
+	return (0);
+}
+
+
