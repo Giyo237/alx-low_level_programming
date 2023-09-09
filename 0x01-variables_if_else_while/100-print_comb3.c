72713e7092f574b54db63d5e583dc6638e489cdf
kalifa <kalifkalifa@gmail.com>
2023-09-08 18:24:37 -0700

    orunt

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,13 +9,17 @@ int main(void)
 	int j;
 
 	for (i = 0; i < 10; i++)
-	for (j = 0; j < 10; j++)
+	{
+	for (j = i+1; j < 10; j++)
+	{
 		putchar(i + '0');
 		putchar(j + '0');
 	if (i != 8 && j != 9)
 	{
 		putchar(',');
 		putchar(' ');
+	}
+	}
 	}
 	putchar('\n');
 return (0);
