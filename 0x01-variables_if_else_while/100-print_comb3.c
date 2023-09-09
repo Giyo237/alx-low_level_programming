4573d7472215d0e87f262df934805a9f16333421
kalifa <kalifkalifa@gmail.com>
2023-09-08 18:36:11 -0700

    pls work

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,19 +8,21 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; i < 10; i++)
+	do {
+	for (i = 0; i <= 8; i++)
 	{
-	for (j = i+1; j < 10; j++)
+	for (j = i + 1; j <= 9; j++)
 	{
 		putchar(i + '0');
 		putchar(j + '0');
-	if (i != 8 && j != 9)
+	if (i != 8 || j != 9)
 	{
 		putchar(',');
 		putchar(' ');
 	}
 	}
 	}
+	} while (i != j);
 	putchar('\n');
 return (0);
 }
