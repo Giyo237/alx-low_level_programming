b7dac9f78ce730d3cbc3870fb7a7c5d29888f013
kalifa <kalifkalifa@gmail.com>
2023-09-08 20:22:49 -0700

    finished'

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,33 +7,22 @@ int main(void)
 {
 	int i;
 	int j;
-	int a;
-	int b;
-	int c;
-	int d;
 
-	for (i = '0'; i <= 99; i++)
+	for (i = 0; i <= 98; i++)
 	{
-		a = i / 10;
-		b = i % 10;
-		for (j = '0'; j <= 99; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
-			c = j / 10;
-			d = j % 10;
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
+		putchar(' ');
+		putchar((j / 10) + '0');
+		putchar((j % 10) + '0');
+		if (i == 98 && j == 99)
+		continue;
+		putchar(',');
+		putchar(' ');
 		}
-			if (a < c || (a == c && b < d))
-			{
-				putchar(a + '0');
-				putchar(b + '0');
-				putchar(' ');
-				putchar(c + '0');
-				putchar(d + '0');
-				if (a != '9' && b != 8)
-				{
-					putchar(',');
-					putchar(' ');
-			}
-	}
-	}
-	return (0);
+		}
+		putchar('\n');
+		return (0):
 }
