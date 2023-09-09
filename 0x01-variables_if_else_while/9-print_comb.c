41ccd30880a2572de7f3e6f0d3bca92dc18a8678
kalifa <kalifkalifa@gmail.com>
2023-09-08 17:07:03 -0700

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,8 +7,8 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 10; i++)
-		putchar(i + '100');
+	for (i = 0; i <= 100; i++)
+		putchar(i + '0');
 	putchar(',');
 		putchar(' ');
 	if ((i + 1) % 10 == 0)
