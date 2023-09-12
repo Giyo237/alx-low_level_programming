d5038cde98244327b226335425685926135c8e3d
kalifa <kalifkalifa@gmail.com>
2023-09-12 16:54:39 -0700

    time for jack

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minutes of the day of jack
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int min;
+	int hrs;
+
+	hrs = 0;
+       min = 0;	
+	for (hrs = 0; hrs < 24; hrs++)
+	{
+	for (min = 0; min < 60; min++)
+	{
+	_putchar((hrs / 10) + '0');
+	_putchar((hrs % 10) + '0');
+	_putchar(':');
+	_putchar((min / 10) + '0');
+	_putchar((min % 10) + '0');
+	_putchar('\n');
+	}
+	}
+}
+
