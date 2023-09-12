e1b1409c5e5a3b331611f51b89369dd7a7ad2438
kalifa <kalifkalifa@gmail.com>
2023-09-12 10:58:31 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - a program that prints numbers
+ * Return: 0
+ */
+void print_alphabet(void);
+{
+	char a;
+
+	for (a = 'a'; a < 'z'; a++)
+	{
+		_putchar('a');
+	}
+	_putchar('\n');
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
