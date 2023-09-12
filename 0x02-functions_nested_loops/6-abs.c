1abbf6e6e4d4b533c20e4aaa319c4d0d7ffc7833
kalifa <kalifkalifa@gmail.com>
2023-09-12 14:57:03 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - printss the abs value of an interger/float
+ * Return: 0
+ */
+int _abs(int a)
+{
+	 if (a < 0)
+	{
+	return (-a);
+	}
+	return (a);
+}
