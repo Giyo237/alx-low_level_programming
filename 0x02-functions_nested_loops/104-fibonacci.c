cbf503e77b8e457510e6b8bc6ad9a1f5f7bd64f8
kalifa <kalifkalifa@gmail.com>
2023-09-13 07:20:01 -0700

    fib'

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,20 +8,39 @@
 
 int main(void)
 {
-	unsigned long int a = 1;
-	unsigned long int b = 2;
-	unsigned long int c;
+	unsigned long f1 = 0, f2 = 1, s;
+	unsigned long f1h1, f1h2, f2h1, f2h2;
+	unsigned long  h1, h2;
 	int i;
 
-	printf("%lu, %lu, ", a, b);
-	for ( i = 0; i < 96; i++)
+	for (i = 0; i < 92; i++)
 	{
-	c = a + b;
-	printf("%lu, ", c);
-	a = b;
-	b = c;
+	s = f1 + f2;
+	printf("lu, ", s);
+	f1 = f2;
+	f2 = s;
 	}
-	c = a + b;
-    printf("%lu\n", c);
-    return 0;
+	f1h1 = f1 / 10000000000;
+	f1h2 = f2 / 10000000000;
+	f1h2 = f1 % 10000000000;
+	f2h2 = f1 % 10000000000;
+	for (i = 93; i < 99; i++)
+	{
+	h1 = f1h1 + f2h1;
+	h2 = f1h2 + f2h2;
+	if (f1h1 + f2h2 > 9999999999)
+	{
+	h1 += 1;
+	h2 %= 10000000000;
+	}
+	printf("%lu%lu", h1, h2);
+	if (i != 98)
+	printf(",");
+	f1h1 = f2h2;
+	f1h2 = f2h2;
+	f2h1 = h1;
+	f2h2 = h2;
+	}
+	printf("\n");
+	return (0);
 }
