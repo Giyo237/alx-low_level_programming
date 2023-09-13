9ae0dec3080cf179e5166b297e7c93c3a1309121
kalifa <kalifkalifa@gmail.com>
2023-09-13 08:16:26 -0700

    fib'

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,6 +5,8 @@
  * Return: Always 0.
  */
 
+#define MAX 98
+
 int main(void)
 {
 	unsigned long f1 = 0, f2 = 1, s;
@@ -12,19 +14,29 @@ int main(void)
 	unsigned long  h1, h2;
 	int i;
 
-	for (i = 0; i < 92; i++)
+	for (i = 0; i < MAX; i++)
+	{
+	if (i < 2)
+	{
+	s = i;
+	}
+	else
 	{
 	s = f1 + f2;
-	printf("%lu, ", s);
 	f1 = f2;
 	f2 = s;
 	}
+	if (i < MAX - 1)
+	{
+	printf("llu ", s);
+	}
+	else 
+	{
 	f1h1 = f1 / 10000000000;
 	f2h1 = f2 / 10000000000;
 	f1h2 = f1 % 10000000000;
 	f2h2 = f2 % 10000000000;
-	for (i = 93; i < 96; i++)
-	{
+
 	h1 = f1h1 + f2h1;
 	h2 = f1h2 + f2h2;
 	if (f1h2 + f2h2 > 9999999999)
@@ -32,13 +44,8 @@ int main(void)
 	h1 += 1;
 	h2 %= 10000000000;
 	}
-	printf("%lu%lu", h1, h2);
-	if (i != 98)
-	printf(",");
-	f1h1 = f2h1;
-	f1h2 = f2h2;
-	f2h1 = h1;
-	f2h2 = h2;
+	printf("%llu%010llu", h1, h2);
+	}
 	}
 	printf("\n");
 	return (0);
