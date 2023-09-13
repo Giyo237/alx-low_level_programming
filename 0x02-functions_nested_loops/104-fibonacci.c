53417588b5b6450292e6270fc43c4452dd8ff985
kalifa <kalifkalifa@gmail.com>
2023-09-13 11:58:26 -0700

    fib

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,42 +5,19 @@
  * Return: Always 0.
  */
 
-#define MAX 98
-
 int main(void)
 {
-	unsigned long f1 = 0, f2 = 1, s;
-	unsigned long f1h1, f1h2, f2h1, f2h2;
-	unsigned long  h1, h2;
-	int i;
+	int n1 = 1, n2 = 2, n3;
 
-	for (i = 0; i <= MAX; i++)
+	printf("1, 2, ");
+	for(int i=3; i<=98; i++)
 	{
-	s = f1 + f2;
-	f1 = f2;
-	f2 = s;
-	if (i <= MAX - 1)
-	{
-	printf("%lu, ", s);
+	n3 = n1 + n2;
+	printf("%d, ", n3);
+	n1 = n2;
+	n2 = n3;
 	}
-	else 
-	{
-	f1h1 = f1 / 10000000000;
-	f2h1 = f2 / 10000000000;
-	f1h2 = f1 % 10000000000;
-	f2h2 = f2 % 10000000000;
 
-	h1 = f1h1 + f2h1;
-	h2 = f1h2 + f2h2;
-	if (f1h2 + f2h2 > 9999999999)
-	{
-	h1 += 1;
-	h2 %= 10000000000;
-	}
-	printf("%lu", h1);
-	printf("%lu", h2);
-	}
-	}
 	printf("\n");
 	return (0);
 }
