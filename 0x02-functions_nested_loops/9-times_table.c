3fa5442ea07105c4d71ba97e04c9ef037544a5af
kalifa <kalifkalifa@gmail.com>
2023-09-12 19:05:05 -0700

    help

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,28 +8,34 @@ void times_table(void)
 	int result;
 	int i;
 	int j;
+	int r;
+	int d;
 
 	for (i = 0; i <= 9; i++)
 	{
 	for (j = 0; j <= 9; j++)
 	{
 	result = i * j;
-	if (result <= 9)
+	if ( result > 9)
 	{
-	_putchar(' ');
-	_putchar(result + '0');
+	r = result % 10;
+	d = ((result - r) / 10);
+	_putchar(44);
+	_putchar(32);
+	_putchar(d + '0');
+	_putchar(r + '0');
 	}
 	else
 	{
-	_putchar((result / 10) + '0');
-	_putchar((result % 10) + '0');
-	}
-	if (j < 9)
+	if (j != 0)
 	{
-	_putchar (',');
-	_putchar(' ');
+	_putchar(44);
+	_putchar(32);
+	_putchar(32);
+	}
+	_putchar(result + '0');
 	}
 	}
 	_putchar('\n');
-}
+	}
 }
