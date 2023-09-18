6c0d94ae206f9646daf86cb9b3e28dc0f35ef22c
kalifa <kalifkalifa@gmail.com>
2023-09-18 16:37:06 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+* rev_string - function that reverses str
+* @s: operant
+*/
+void rev_string(char *s)
+{
+	int l = strlen(s);
+	int i, j;
+	char temp;
+
+	for (i = 0, j = l - 1;i < j;i++, j--)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+	}
+}
