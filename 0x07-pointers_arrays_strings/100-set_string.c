6d653d69145e50cb8fe66685156a19f6cf35d162
kalifa <kalifkalifa@gmail.com>
2023-09-25 11:51:43 -0700

    prt to prt

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,7 +2,7 @@
 /**
 * set_string - function that sets pointer to char
 * @s: the prt to the prt of a char
-* @t: the prt to the char
+* @to: the prt to the char
 * Return: 0 if succesful
 */
 void set_string(char **s, char *to)
