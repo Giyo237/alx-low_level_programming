1d69f4bfb71a6e6f7c6c8b1779f951064147358e
kalifa <kalifkalifa@gmail.com>
2023-09-25 11:50:07 -0700

    prt to prt

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* set_string - function that sets pointer to char
+* @s: the prt to the prt of a char
+* @t: the prt to the char
+* Return: 0 if succesful
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define even_more_pointers
 
 char *_memset(char *s, char b, unsigned int n);
+void set_string(char **s, char *to);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
