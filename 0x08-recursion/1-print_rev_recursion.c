8f99c76a6dbef2827986d22aac40ff38dd50b7ce
kalifa <kalifkalifa@gmail.com>
2023-09-28 07:42:47 -0700

    rev string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - function that prints strng in rev
+ * @s: starting point
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
