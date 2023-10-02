40c5bc6e0771c87441275a899d2d413955e1beb0
kalifa <kalifkalifa@gmail.com>
2023-10-02 12:21:22 -0700

    static lib

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - printss the abs value of an interger/float
+ * Return: 0
+ *@a: checker value
+ */
+int _abs(int a)
+{
+	if (a < 0)
+	{
+	return (-a);
+	}
+	return (a);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+standaed libraries
