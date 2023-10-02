f437a0255007f93d240ff7ba81c73071cd1b0f0f
kalifa <kalifkalifa@gmail.com>
2023-10-02 12:43:02 -0700

    task0

---
+++ 0x09-static_libraries/prep/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase letters
+ * @c: the operant of the code
+ * Return: either 1 or c if succesful otherwise return 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+
+	return (0);
+}
