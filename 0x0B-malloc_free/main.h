2ca19acd0ac03cf61fd610f926259333979d2d93
kalifa <kalifkalifa@gmail.com>
2023-10-03 12:53:36 -0700

    two in one task

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,8 @@
 #ifndef malloc_free
 #define malloc_free
 
+char **strtow(char *str);
+char *argstostr(int ac, char **av);
  char *create_array(unsigned int size, char c);
  char *_strdup(char *str);
  char *str_concat(char *s1, char *s2);
