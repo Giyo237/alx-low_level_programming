39b0af601fd1f2e209185b83cb4d64240f8e75fd
kalifa <kalifkalifa@gmail.com>
2023-10-09 12:08:48 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -35,7 +35,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	 p = ptr, q = nptr;
 	if (new_size < old_size)
 	{
-		for (i = 0; i < new_size;i++)
+		for (i = 0; i < new_size; i++)
 			q[i] = p[i];
 	}
 
