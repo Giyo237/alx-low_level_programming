651da1cf182242a2f82191798da11c6084d41276
kalifa <kalifkalifa@gmail.com>
2023-10-10 06:06:01 -0700

    my dog name

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * struct dog - a structure that collects data of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: of the dog
+ * this structure helps collectt data of a dog with the help of
+ * variables name age owner
+ * the name and owner fields are ponters to character array
+ * which is use to store strings
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*MAIN_H*/
