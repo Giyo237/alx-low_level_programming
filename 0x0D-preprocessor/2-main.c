2e40717af84dd8c49d8e64c9ebd2b77a85d47c77
kalifa <kalifkalifa@gmail.com>
2023-10-10 05:00:57 -0700

    source co,piler name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,32 +1,11 @@
 #include <stdio.h>
-#include <unistd.h>
-
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * main -function that prints the name of the compiled file
+ * Return: 0
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-
-/**
-*main - a function that pronts the name of the file it was compiles from
-* Return: 0 if succeful
-*/
 int main(void)
 {
-	const char *file = __FILE__;
+	printf("%s\n", __FILE__);
 
-	while (*file != '\0')
-	{
-		_putchar(*file);
-		file++;
-	}
-	_putchar('\n');
 	return (0);
 }
