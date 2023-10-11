6b5e8f506c11c30900b02b8fdcb8fe8e19bcde52
kalifa <kalifkalifa@gmail.com>
2023-10-11 08:01:21 -0700

    pointer to function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,17 +8,17 @@
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array != NULL || action != NULL)
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+	else
 	{
 		unsigned int i;
-	
+
 		for (i = 0; i < size; i++)
 		{
 			action(array[i]);
 		}
-	}
-	else
-	{
-		return;
 	}
 }
