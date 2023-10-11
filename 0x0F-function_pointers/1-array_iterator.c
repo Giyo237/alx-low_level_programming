824724915993d1db1caa9c236f7da2d68ead6d77
kalifa <kalifkalifa@gmail.com>
2023-10-11 07:51:50 -0700

    array task completed

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+* array_iterator - function that exe a function as para
+* @array: pointer to the array of iterator
+* @size: the size of the arr
+* @action: pointer to the function
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array != NULL && action != NULL)
+	{
+		unsigned int i;
+	
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
