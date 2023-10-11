d10e9b06b657ad724f7ef0f0407c8a5c842e3ee3
kalifa <kalifkalifa@gmail.com>
2023-10-11 11:13:09 -0700

    alx

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,7 +9,6 @@
 int main(int argc, char *argv[])
 {
 	int bytes, i;
-/*	unsigned char main_ptr;*/
 
 	if (argc != 2)
 	{
@@ -19,16 +18,17 @@ int main(int argc, char *argv[])
 
 	bytes = atoi(argv[1]);
 
-	if (bytes <= 0)
+	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	char *arr = (char *)&main;
+
+	char *arr = (char *)main;
 
 	for (i = 0; i < bytes; i++)
 	{
-		if(i == bytes - 1)
+		if (i == bytes - 1)
 		{
 		printf("%02hhx\n ", arr[i]);
 		break;
