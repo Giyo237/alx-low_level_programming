ec383062af20e6fe9dbeefd6f88e9501118b90e1
kalifa <kalifkalifa@gmail.com>
2023-10-11 10:57:41 -0700

    opcode

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,7 +9,7 @@
 int main(int argc, char *argv[])
 {
 	int bytes, i;
-	char *a;
+/*	unsigned char main_ptr;*/
 
 	if (argc != 2)
 	{
@@ -19,22 +19,19 @@ int main(int argc, char *argv[])
 
 	bytes = atoi(argv[1]);
 
-	if (bytes < 0)
+	if (bytes <= 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-
-	a = (char *)main;
+	unsigned char *main_ptr = (unsigned char *)&main;
 
 	for (i = 0; i < bytes; i++)
 	{
-		if (i == bytes - 1)
-		{
-			printf("%02hhx\n", a[i]);
-			break;
-		}
-		printf("%02hhx", a[i]);
+		printf("%02x ", main_ptr[i]);
 	}
+
+	printf("\n");
+
 	return (0);
 }
