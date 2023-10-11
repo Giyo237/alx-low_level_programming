566297013a32b5674147e7a9acd88f9be6bed584
kalifa <kalifkalifa@gmail.com>
2023-10-11 07:38:42 -0700

    bob

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
-#include <stddef.h>
 #ifndef FUNCTION_POINTER
 #define FUNCTION_POINTER
+
+#include <stddef.h>
+#include <stdllib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
