616f1f57e6ebacf599d4c66dc6514425746c2514
kalifa <kalifkalifa@gmail.com>
2023-10-11 11:08:42 -0700

    opcode

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -24,14 +24,16 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(2);
 	}
-	unsigned char *main_ptr = (unsigned char *)&main;
+	char *arr = (char *)&main;
 
 	for (i = 0; i < bytes; i++)
 	{
-		printf("%02x ", main_ptr[i]);
+		if(i == bytes - 1)
+		{
+		printf("%02hhx\n ", arr[i]);
+		break;
+		}
+		printf("%02hhx ", arr[i]);
 	}
-
-	printf("\n");
-
 	return (0);
 }
