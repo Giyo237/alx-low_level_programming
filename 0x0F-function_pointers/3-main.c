7faba5006da02db53e1f5d15242bf0fe5b9a0f34
kalifa <kalifkalifa@gmail.com>
2023-10-11 10:18:37 -0700

    calculatot

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -26,8 +26,8 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*op == '/' && num2 == 0) 
-	|| (*op == '%' && num2 == 0))			
+	if ((*op == '/' && num2 == 0)
+	|| (*op == '%' && num2 == 0))		
 	{
 		printf("Error\n");
 		exit(100);
