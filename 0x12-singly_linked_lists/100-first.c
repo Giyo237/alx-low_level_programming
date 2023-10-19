e47d3501995cb02b37e9e7aa748a0dce0b5af665
kalifa <kalifkalifa@gmail.com>
2023-10-19 16:57:10 -0700

    advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  * print_this_first - a function that prints the
+  *bellow qoute b4 the code is executed
+  */
+void __attribute__((constructor)) print_this_first(void)
+{
+	printf("You'beat! and yet, you must allow,\n");
+	printf("bore my house upon my back!\n");
+}
