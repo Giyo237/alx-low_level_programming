86da0c9bdfe609af7dc6a3ef79e3e18aa836a32c
kalifa <kalifkalifa@gmail.com>
2023-10-19 16:45:39 -0700

    freed

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free__list - function that rfrees a list_t
+ *
+ * @head: Thepointer to the first node
+ * 
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *temp = head;
+	list_t *next;
+
+	while (temp != NULL)
+	{
+		next = temp->next;
+		free(temp->str);
+		free(temp);
+		temp = next;
+	}
+}
