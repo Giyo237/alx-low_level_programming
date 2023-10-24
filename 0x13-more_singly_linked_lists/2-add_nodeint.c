c3486b63dd4e73298daa7e9ac8d210ba5f26e320
kalifa <kalifkalifa@gmail.com>
2023-10-24 14:41:59 -0700

    add node to listint_t

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- *add_nodeint - function that returns address of new node
- *
- * @h:pointer to the pinter of the head
- * @n: value to add to the new node
- * Return: address
- */
- listint_t *add_nodeint(listint_t **head, const int n)
+*add_nodeint - function that returns address of new node
+*
+* @head:pointer to the pinter of the head
+* @n: value to add to the new node
+* Return: address
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *nnode;
 
