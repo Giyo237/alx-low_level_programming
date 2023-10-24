839a253fb1eaa66eb9c044c89f2532b0299ff295
kalifa <kalifkalifa@gmail.com>
2023-10-24 15:09:46 -0700

    free them

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint - function that frees listint_t
+ *
+ * head: pointer to the head of the list
+ *
+ * 
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *now;
+
+	while (head)
+	{
+		now = head;
+		head = head->next;
+		free(now);
+	}
+}
