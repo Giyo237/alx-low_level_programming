fce7352441664040be1708460562b6698acc30ac
kalifa <kalifkalifa@gmail.com>
2023-10-24 15:12:07 -0700

    free them

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -3,9 +3,9 @@
 /**
  * free_listint - function that frees listint_t
  *
- * head: pointer to the head of the list
+ * @head: pointer to the head of the list
  *
- * 
+ *
  */
 void free_listint(listint_t *head)
 {
