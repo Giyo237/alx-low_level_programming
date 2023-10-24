62bd0c79278718f9e1e639624da01073b2ca239a
kalifa <kalifkalifa@gmail.com>
2023-10-24 16:44:54 -0700

    idx

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,15 +24,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		nnode->n = n;
 		nnode->next = *head;
 		*head = nnode;
-	}
 		return (nnode);
+	}
 		for (i = 0; now != NULL && i < idx - 1; i++)
 			now = now->next;
 		if (now == NULL)
+		{
 			return (NULL);
+		}
 		nnode = malloc(sizeof(listint_t));
 		if (nnode == NULL)
+		{
 			return (NULL);
+		}
 		nnode->n = n;
 		nnode->next = now->next;
 		now->next = nnode;
