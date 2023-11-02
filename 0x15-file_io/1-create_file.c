6e552a78bb43f1651e4229ff0010ba8a076adac8
kalifa <kalifkalifa@gmail.com>
2023-11-02 10:36:11 -0700

    task 1[

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,25 +14,16 @@ int create_file(const char *filename, char *text_content)
 	int t, counter, wr;
 
 	if (!filename)
-	{
 		return (-1);
-	}
 	t = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (t == -1)
-	{
 		return (-1);
-	}
-
 	if (!text_content)
-	{
 		text_content = "";
-	}
 	for (counter = 0; text_content[counter]; counter++)
 		wr = write(t, text_content, counter);
 	if (wr == -1)
-	{
 		return (-1);
-	}
 	close(t);
 	return (1);
 }
