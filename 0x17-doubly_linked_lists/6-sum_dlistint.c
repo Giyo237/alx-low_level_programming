cc1f3efaf69d1e5c2abdc328a85a778257373376
kalifa <kalifkalifa@gmail.com>
2023-12-07 16:05:23 -0800

    task 6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ *sum_dlistin  - function that returns all data of a dlistint
+ *
+ * @head: The first element of the linked list
+ *
+ * Return:sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int adder = 0;
+
+	while (head != NULL)
+	{
+		adder += head->n;
+		head = head->next;
+	}
+	return (adder);
+}
+
